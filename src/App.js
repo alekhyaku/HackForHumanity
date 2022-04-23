3880c683a897852b1bb9605f63992e5d3dc380c3
alekhyaku <a1.kuchimanchi@gmail.com>
2022-04-23 17:38:55 -0500

    changing the images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from "./logo.svg";
+import disaster from "./image/disaster.jpg"
+import "./image/blueperi.jpg"
+import "./image/distrash.jpg"
 import "./App.css";
+import "./image/natDisBig.jpg"
+import "./image/realremove.png"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />*/}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <code> Find resources near you </code>
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in 5 minutes
+          {/* Learn React in 5 minutes */}
         </a>
       </header>
     </div>
